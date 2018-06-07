5b6f8f2456309bf8b5b91022730fdc20b4ccb7a0
Sneha Dama <snehadama@Snehas-Air.lan>
2018-06-07 15:09:36 -0500

    Scrape works

--- server.js
+++ server.js
@@ -63,70 +63,29 @@ mongoose.connect(db,function(error){
 // A GET route for scraping the Medium.com website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with request
-    axios.get("https://medium.com/").then(function(response) {
+    request("https://medium.com/", function (error, response, body) {
         // console.log(response);
       // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-      console.log(response.data);
+      var $ = cheerio.load(body);
+    //   console.log(response.data);
 
+    var articles = [];
       // Now, we grab every h3 within an article tag, and do the following:
     $(".extremeHero-postContent h3").each(function(i, element) {
 
-        var articles = {};
-
         var articleTitle = element.children[0].data;
-        console.log(articleTitle);
+        console.log("title", articleTitle);
   
         var articleLink = element.parent.attribs.href;
-        console.log(articleLink);
+        console.log("link", articleLink);
   
         articles.push(`<a href= "${articleLink}">${articleTitle}</a>`);
-
-        // var result = {};
-
-        // result.title = $(this)
-        // .children("a")
-        // .text();
-
-        // result.link = $(this)
-        // .children("a")
-        // .attr("href");
-        // console.log("hello");
-        // console.log(element);
-
-
+    });
     
-        // Save an empty articles object
-        // var articles = {};
-
-        // var articleTitle = element.children[0].data;
-        // console.log(articleTitle);
-  
-        // var articleLink = element.parent.attribs.href;
-        // console.log(articleLink);
-  
-        // articles.push(`<a href= "${articleLink}">${articleTitle}</a>`);
-        
-        //Creating a new article using the articleTitle.
-    //     db.Article.create(articleTitle)
-    //        .then(function(dbArticle){
-    //            console.log(dbArticle);
-    //        })
-    //        .catch(function(err){
-    //            return res.json(err);
-    //        });
-
     res.send(articles.join("<br>"));
 
     });
-
-    
-
-  });
-
-
 });
-
 
 
 // Start the server
