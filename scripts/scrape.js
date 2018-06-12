f50d99872698935e1408fe6959d1bb939b6786a2
Sneha Dama <snehadama@Snehas-Air.lan>
2018-06-12 17:02:17 -0500

    Edited css

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -25,9 +25,6 @@
 
 // };
 
-// module.exports = scrape;
-
-
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
@@ -37,12 +34,12 @@ var scrape = function(callback) {
 
   var articlesArr = [];
 
-  request("https://www.nytimes.com/", function(error, response, html) {
+  request("https://medium.com/", function(error, response, html) {
 
       var $ = cheerio.load(html);
 
 
-      $("h2.story-heading").each(function(i, element) {
+      $("extremeHero-postContent h3").each(function(i, element) {
 
           var result = {};
 
@@ -58,5 +55,7 @@ var scrape = function(callback) {
   });
 
 };
+
+module.exports = scrape;
 
 module.exports = scrape;
